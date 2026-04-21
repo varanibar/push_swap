98af135b2cac6019d2e3d76e79fe1a2b71c7ab71
Veronica Aranibar Flores <mymindisfloating@gmail.com>
2026-04-21 20:20:45 +0200

    added comments to group functions

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: varaniba <varaniba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/20 12:26:26 by varaniba      #+#    #+#                 */
-/*   Updated: 2026/04/20 18:32:35 by varaniba      ########   odam.nl         */
+/*   Updated: 2026/04/21 17:52:34 by varaniba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,26 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-int					ft_atoi(const char *nptr);
+//validating input
+int					ft_check_input(int argc, char **argv);
+
+//Defining the algorithm to use
+double				ft_compute_disorder(t_list *stack);
+
+//adding nodes to stack
 int					ft_add_to_stack(t_list **stack, int val);
+
+//operations
 void				del_top(t_list **stack);
-void				print_stack(t_list *stack);
 void				swap(t_list **stack);
 void				rotate(t_list **stack);
 void				reverse_rotate(t_list **stack);
-int					ft_check_input(char *input);
-double				compute_disorder(t_list *stack);
+
+//printing stack
+void				print_stack(t_list *stack);
+
+//helper functions (we should fetch them from libft)
+int					ft_atoi(const char *nptr);
 int					ft_lstsize(t_list *lst);
+
 #endif
