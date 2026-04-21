60a088bc5a863ea344a7d14429e10b5ff25d42b8
Veronica Aranibar Flores <mymindisfloating@gmail.com>
2026-04-21 20:22:47 +0200

    modified function, added the flags checker and ft_split for a specific type of input

--- ft_check_input.c
+++ ft_check_input.c
@@ -6,87 +6,122 @@
 /*   By: lekoelma <lekoelma@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/20 15:36:55 by lekoelma      #+#    #+#                 */
-/*   Updated: 2026/04/20 19:24:35 by varaniba      ########   odam.nl         */
+/*   Updated: 2026/04/21 20:20:12 by varaniba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
-
-int	ft_check_isdigit(char *str)
+// #include "push_swap.h"
+#include <stdio.h>
+#include "libft.h"
+//check if we have valid characters in the string
+//we should only have integers, one sing
+static int	ft_is_str_valid(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (str[i] == '-')
+	if (str[i] == '+' || str[i] == '-')
 		i++;
-	while (str[i])
-	{
-		if (str[i] < '0' || str[i] > '9')
-			return (0);
+	while ((str[i] >= '0' && str[i] <= '9'))
 		i++;
-	}
-	return (1);
+	if (str[i] == '\0')
+		return (1);
+	return (0);
 }
 
-int	ft_check_dup(char **input)
+// //check for duplicates
+// static int	ft_check_dup(char **input)
+// {
+// 	int	i;
+// 	int	j;
+
+// 	i = 0;
+// 	while (input[i])
+// 	{
+// 		j = i + 1;
+// 		while (input[j])
+// 		{
+// 			if (ft_atoi(input[i]) == ft_atoi(input[j]))
+// 				return (0);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// 	return (1);
+// }
+//There if something isnt valid, this function will return 1 which
+//will signal to the whole program that it needs to stop
+//There are three possible cases :
+//	-argc is one, no arguments to work with, error
+//	-argc is two, then the second argument must be the argument
+//	   that contains all the numbers in a string, so we need to first
+//	   split it using ft split and then pass the split arguments to
+//	   the next check. If it is not this string (like just a flag)
+//	   then error.
+//	-argc is more than two, the point to check here is that we can
+//	   maybe have argv[1] and argv[2] as flags, so in case those
+//	   arent valid, we need to see if they are not valid because
+//	   they are actually flags. we need to use something like strcmp
+
+
+static int ft_strcmp_flags(const char *str)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	*flags[5] = {"--bench", "--simple", "--medium", "--complex", "--adaptative"};
 
 	i = 0;
-	while (input[i])
+	j = 0;
+	while (j < 5)
 	{
-		j = i + 1;
-		while (input[j])
+		while (str[i] != '\0' || flags[j][i] != '\0' )
 		{
-			if (ft_atoi(input[i]) == ft_atoi(input[j]))
-				return (0);
-			j++;
+			if (str[i] != flags[j][i])
+				break ;
+			i++;
 		}
-		i++;
+		if (str[i] == '\0' || flags[j][i] == '\0' )
+			return(1);
+		j++;
+		i=0;
 	}
-	return (1);
+	return (0);
 }
 
-int	ft_check_input(char *input)
+int	ft_check_input(int argc, char **argv)
 {
 	int	i;
+	char **tmp;
 
 	i = 0;
-	if (input[i] == '\0')
-		return(0);
-	while (input[i])
+	tmp = NULL;
+	if (argc == 1 || (argc == 2 && argv[1][0] == '\0'))
+		return (write(1, "Error 1\n", 8), 0);
+	else if (argc == 2)
 	{
-		if (!ft_check_isdigit(input[i]) || !ft_check_dup(input[i]))
-			return (0);
-		i++;
+		tmp = ft_split(argv[1], ' ');
+		if (!tmp || tmp[0] == NULL)
+			return(write(1, "Error 2\n", 8), 0);
+		while (tmp[i] != NULL)
+			if (!ft_is_str_valid(tmp[i++]))
+				return (write(1, "Error 3\n", 8), 0);
 	}
+	else
+		while (++i < argc)
+			if (!ft_is_str_valid(argv[i]))
+				if(!ft_strcmp_flags(argv[i]))
+					return (write(1, "Error 4\n", 8), 0);
 	return (1);
 }
 
+//Testing main for this function
 int	main(int argc, char **argv)
 {
-	t_list *stack_a;
-	t_list *stack_b = NULL;
-	int input[argc];
-	int i;
-	int j;
-	i = 0;
-	j = 1;
-	stack_a = NULL;
-	if (argc == 1)
-	{
-		write(1, "Error\n", 6);
+	//one function should verify all the conditions for all the
+	//arguments, otherwise one argument might not pass the
+	//check and it still might work
+
+	if (!ft_check_input(argc, argv))
 		return (0);
-	}
-	while (argv[j])
-	{
-		if (!ft_check_input(argv[j]))
-		{
-			printf("%s\n", "Error");
-			return(0);
-		}
-		j++;
-	}
 	return(0);
 }
