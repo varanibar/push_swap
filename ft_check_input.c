52de7d5e133189cc5f87f1b789fb7487de3c770f
Lev Koelman <lekoelma@f1r4s7.codam.nl>
2026-04-22 16:54:51 +0200

    added add, check, disorder

---
+++ ft_check_input.c
@@ -0,0 +1,151 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_check_input.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lekoelma <lekoelma@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/04/20 15:36:55 by lekoelma      #+#    #+#                 */
+/*   Updated: 2026/04/22 10:18:23 by varaniba      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "libft.h"
+//check if we have valid characters in the string
+//we should only have integers, one sing
+static int	ft_is_str_valid(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while ((str[i] >= '0' && str[i] <= '9'))
+		i++;
+	if (str[i] == '\0')
+		return (1);
+	return (0);
+}
+
+//check for duplicates
+static int	ft_check_dup(char **input)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (input[i])
+	{
+		j = i + 1;
+		while (input[j])
+		{
+			if (ft_atoi(input[i]) == ft_atoi(input[j]))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+//There if something isnt valid, this function will return 1 which
+//will signal to the whole program that it needs to stop
+//There are three possible cases :
+//	-argc is one, no arguments to work with, error
+//	-argc is two, then the second argument must be the argument
+//	   that contains all the numbers in a string, so we need to first
+//	   split it using ft split and then pass the split arguments to
+//	   the next check. If it is not this string (like just a flag)
+//	   then error.
+//	-argc is more than two, the point to check here is that we can
+//	   maybe have argv[1] and argv[2] as flags, so in case those
+//	   arent valid, we need to see if they are not valid because
+//	   they are actually flags. we need to use something like strcmp
+
+
+static int ft_flag_checker(char **argv)
+{
+	int		i;
+	int		j;
+	char	*flags[5] = {"--bench", "--simple", "--medium", "--complex", "--adaptative"};
+	int k;
+	i = 0;
+	j = 0;
+	k = 1;
+	while (j < 5 && k < 3)
+	{
+		while (argv[k][i] != '\0' || flags[j][i] != '\0' )
+		{
+			if (argv[k][i] != flags[j][i])
+				break ;
+			i++;
+		}
+		if (argv[k][i] == '\0' && flags[j][i] == '\0' )
+			k++;
+		j++;
+		i = 0;
+	}
+	return (k - 1);
+}
+
+static int	ft_check_input_split(char *argv_1)
+{
+	int	i;
+	char **input;
+
+	i = 0;
+	input = ft_split(argv_1, ' ');
+	if (!input || input[0] == NULL)
+		return(ft_printf("Error in split : ft_split didnt work or full of ' ' passed as argument\n"), 0);
+	while (input[i] != NULL)
+	{
+		if (!ft_is_str_valid(input[i]))
+			return (ft_printf("Error in split : invalid values\n"), 0);
+		i++;
+	}
+	if (!ft_check_dup(input))
+		return (ft_printf("Error in split : duplicates\n"), 0);
+	return(1);
+}
+
+int	ft_check_input(int argc, char **argv)
+{
+	int	i;
+	int flags;
+
+	i = 1;
+	flags = 0;
+	if (argc == 1 || (argc == 2 && argv[1][0] == '\0'))
+		return (ft_printf("Error in first condition\n"), 0);
+	else if (argc == 2)
+	{
+		if (!ft_check_input_split(argv[1]))
+			return (0);
+	}
+	else
+	{
+		flags += ft_flag_checker(argv);
+		i += flags;
+		if (i == argc)
+			return(ft_printf("Error : Only flags present\n"), 0);
+		while (i < argc)
+			if (!ft_is_str_valid(argv[i++]))
+					return (ft_printf("Error : invalid values\n"), 0);
+		if (!ft_check_dup(argv + 1 + flags))
+			return (ft_printf("Error : duplicates \n"), 0);
+	}
+	return (1);
+}
+
+
+// Testing main for this function
+// int	main(int argc, char **argv)
+// {
+// 	//one function should verify all the conditions for all the
+// 	//arguments, otherwise one argument might not pass the
+// 	//check and it still might work
+
+// 	if (!ft_check_input(argc, argv))
+// 		return (ft_printf("Error\n"), 0);
+// 	return(0);
+// }
--- ft_reverse_operation.c
+++ ft_reverse_operation.c
@@ -6,7 +6,7 @@
 /*   By: lekoelma <lekoelma@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/22 14:17:58 by lekoelma      #+#    #+#                 */
-/*   Updated: 2026/04/22 14:53:13 by lekoelma      ########   odam.nl         */
+/*   Updated: 2026/04/22 16:45:50 by lekoelma      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,32 +48,32 @@ void	rr(t_stack **stack_a, t_stack **stack_b)
 	rotate(stack_b);
 	ft_printf("%s\n", "rr");
 }
-// int	main(void)
-// {
-// 	t_stack	*stack_a;
-// 	t_stack	*stack_b;
-// 	int		i;
-// 	int		j;
+int	main(void)
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	int		i;
+	int		j;
 
-// 	stack_a = NULL;
-// 	stack_b = NULL;
-// 	i = 4;
-// 	j = 8;
-// 	while (i > 0)
-// 	{
-// 		ft_add_to_stack(&stack_a, i);
-// 		ft_add_to_stack(&stack_b, j);
-// 		i--;
-// 		j--;
-// 	}
-// 	ft_printf("%s\n", "stack_a before operation:");
-// 	print_stack(stack_a);
-// 	ft_printf("%s\n", "stack_b before operation:");
-// 	print_stack(stack_b);
-// 	rr(&stack_a, &stack_b);
-// 	ft_printf("%s\n", "stack_a after operation:");
-// 	print_stack(stack_a);
-// 	ft_printf("%s\n", "stack_b after operation:");
-// 	print_stack(stack_b);
-// 	return (0);
-// }
+	stack_a = NULL;
+	stack_b = NULL;
+	i = 4;
+	j = 8;
+	while (i > 0)
+	{
+		ft_add_to_stack(&stack_a, i);
+		ft_add_to_stack(&stack_b, j);
+		i--;
+		j--;
+	}
+	ft_printf("%s\n", "stack_a before operation:");
+	print_stack(stack_a);
+	ft_printf("%s\n", "stack_b before operation:");
+	print_stack(stack_b);
+	rr(&stack_a, &stack_b);
+	ft_printf("%s\n", "stack_a after operation:");
+	print_stack(stack_a);
+	ft_printf("%s\n", "stack_b after operation:");
+	print_stack(stack_b);
+	return (0);
+}
