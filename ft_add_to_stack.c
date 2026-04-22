8aad16b91932d8abde15497adeb896c121ff7ae8
Veronica Aranibar Flores <varaniba@f1r4s9.codam.nl>
2026-04-22 10:20:50 +0200

    changed type to t_stack

--- ft_add_to_stack.c
+++ ft_add_to_stack.c
@@ -6,17 +6,17 @@
 /*   By: varaniba <varaniba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/20 14:14:58 by varaniba      #+#    #+#                 */
-/*   Updated: 2026/04/20 18:15:31 by varaniba      ########   odam.nl         */
+/*   Updated: 2026/04/22 09:13:52 by varaniba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_add_to_stack(t_list **stack, int val)
+int	ft_add_to_stack(t_stack **stack, int val)
 {
-	t_list	*new_node;
+	t_stack	*new_node;
 
-	new_node = malloc(sizeof(t_list));
+	new_node = malloc(sizeof(t_stack));
 	if (!new_node)
 		return(-1);
 	new_node->val = val;
