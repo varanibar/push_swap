03ade8038de4c72aca80474cf60a2bf9ad6ef882
Veronica Aranibar Flores <varaniba@f1r4s9.codam.nl>
2026-04-22 10:23:04 +0200

    testing helper functions in main

--- push_swap.c
+++ push_swap.c
@@ -6,102 +6,69 @@
 /*   By: varaniba <varaniba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/18 13:51:14 by varaniba      #+#    #+#                 */
-/*   Updated: 2026/04/20 19:24:41 by varaniba      ########   odam.nl         */
+/*   Updated: 2026/04/22 09:18:18 by varaniba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libft.h"
 
-int	ft_atoi(const char *nptr)
+void	print_stack(t_stack *stack)
 {
-	int	i;
-	int	nb;
-	int	sign;
-
-	sign = 1;
-	i = 0;
-	nb = 0;
-	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
-		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		nb = nb * 10 + (nptr[i] - '0');
-		i++;
-	}
-	return (nb * sign);
-}
-
-void	print_stack(t_list *stack)
-{
-	t_list	*current;
+	t_stack	*current;
 
 	current = stack;
 	while (current != NULL)
 	{
-		printf("%d\n", current->val);
+		ft_printf("%d\n", current->val);
 		current = current->next;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	t_list *stack_a;
-	t_list *stack_b = NULL;
-	int input[argc];
-	int i;
-	int j;
-	i = 0;
-	j = 1;
-	stack_a = NULL;
-	if (argc == 1)
-	{
-		write(1, "Error\n", 6);
-		return (0);
-	}
-	while (argv[j])
-	{
-		if (!ft_check_input(argv[j]))
-		{
-			printf("%s\n", "Error");
-			return(0);
-		}
-		j++;
-	}
-	j = argc - 1;
-	while (j > 0)
-	{
-		input[i] = ft_atoi(argv[j]);
-		if (new_node(&stack_a, input[i]) == -1)
-			return(write(1, "error", 5));
-		i++;
-		j--;
-	}
+	// t_stack *stack_a;
+	// t_stack *stack_b = NULL;
+	// int input[argc];
+	// int i;
+	// int j;
+	// i = 0;
+	// j = 1;
+	// stack_a = NULL;
 
-	double disorder;
-	disorder = compute_disorder(stack_a);
-	printf("disorder = %f\n\n", disorder);
+	if (!ft_check_input(argc, argv))
+		return(ft_printf("%s\n", "Error"), 0);
 
-	printf("%s\n", "Stack before operation:");
-	print_stack(stack_a);
-	// rotate(&stack_a);
 
-	// printf("%s\n", "Stack after operation:");
+	// j = argc - 1;
+	// while (j > 0)
+	// {
+	// 	input[i] = ft_atoi(argv[j]);
+	// 	if (new_node(&stack_a, input[i]) == -1)
+	// 		return(write(1, "error", 5));
+	// 	i++;
+	// 	j--;
+	// }
 
+	// double disorder;
+	// disorder = ft_compute_disorder(stack_a);
+	// printf("disorder = %f\n\n", disorder);
+
+	// printf("%s\n", "Stack before operation:");
 	// print_stack(stack_a);
+	// // rotate(&stack_a);
 
-	//moving first node to stack b
-	stack_b = stack_a;
-	stack_a = stack_a->next;
-	stack_b->next = NULL;
-	printf("%s\n", "Stack a:");
-	print_stack(stack_a);
-	printf("%s\n", "Stack b:");
-	print_stack(stack_b);
+	// // printf("%s\n", "Stack after operation:");
+
+	// // print_stack(stack_a);
+
+	// //moving first node to stack b
+	// stack_b = stack_a;
+	// stack_a = stack_a->next;
+	// stack_b->next = NULL;
+	// printf("%s\n", "Stack a:");
+	// print_stack(stack_a);
+	// printf("%s\n", "Stack b:");
+	// print_stack(stack_b);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: varaniba <varaniba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/20 12:26:26 by varaniba      #+#    #+#                 */
-/*   Updated: 2026/04/21 17:52:34 by varaniba      ########   odam.nl         */
+/*   Updated: 2026/04/22 10:18:33 by varaniba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,32 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdio.h>
 
-typedef struct s_list
+typedef struct s_stack
 {
 	int				val;
-	struct s_list	*next;
-}					t_list;
+	struct s_stack	*next;
+}					t_stack;
 
 //validating input
 int					ft_check_input(int argc, char **argv);
 
 //Defining the algorithm to use
-double				ft_compute_disorder(t_list *stack);
+double				ft_compute_disorder(t_stack *stack);
 
 //adding nodes to stack
-int					ft_add_to_stack(t_list **stack, int val);
+int					ft_add_to_stack(t_stack **stack, int val);
 
 //operations
-void				del_top(t_list **stack);
-void				swap(t_list **stack);
-void				rotate(t_list **stack);
-void				reverse_rotate(t_list **stack);
+void				del_top(t_stack **stack);
+void				swap(t_stack **stack);
+void				rotate(t_stack **stack);
+void				reverse_rotate(t_stack **stack);
 
 //printing stack
-void				print_stack(t_list *stack);
+void				print_stack(t_stack *stack);
 
 //helper functions (we should fetch them from libft)
-int					ft_atoi(const char *nptr);
-int					ft_lstsize(t_list *lst);
+// int					ft_atoi(const char *nptr);
 
 #endif
