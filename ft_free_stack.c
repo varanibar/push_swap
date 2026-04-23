e352807cf0b24a792a12f41c159bf06af2640bb9
Veronica Aranibar Flores <varaniba@f1r4s4.codam.nl>
2026-04-23 17:25:58 +0200

    changed name of function

---
+++ ft_free_stack.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_free_stack.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: varaniba <varaniba@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/04/23 17:04:51 by varaniba      #+#    #+#                 */
+/*   Updated: 2026/04/23 17:07:18 by varaniba      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "libft.h"
+
+void	ft_free_stack(t_stack **stack)
+{
+	t_stack	*tmp;
+
+	if (!stack || !(*stack))
+		return ;
+	tmp = NULL;
+	while (*stack != NULL)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+}
