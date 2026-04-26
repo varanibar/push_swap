a61608e8d0ef9e20a514978d717988886fa6d05e
Veronica Aranibar Flores <varaniba@f1r2s18.codam.nl>
2026-04-26 13:46:00 +0200

    main: check input verification added

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: varaniba <varaniba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/18 13:51:14 by varaniba      #+#    #+#                 */
-/*   Updated: 2026/04/22 09:18:18 by varaniba      ########   odam.nl         */
+/*   Updated: 2026/04/26 12:14:52 by varaniba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,48 +27,19 @@ void	print_stack(t_stack *stack)
 
 int	main(int argc, char **argv)
 {
-	// t_stack *stack_a;
-	// t_stack *stack_b = NULL;
-	// int input[argc];
-	// int i;
-	// int j;
-	// i = 0;
-	// j = 1;
-	// stack_a = NULL;
+	t_stack	*stack_a;
+	t_flags	*flags;
 
-	if (!ft_check_input(argc, argv))
-		return(ft_printf("%s\n", "Error"), 0);
-
-
-	// j = argc - 1;
-	// while (j > 0)
-	// {
-	// 	input[i] = ft_atoi(argv[j]);
-	// 	if (new_node(&stack_a, input[i]) == -1)
-	// 		return(write(1, "error", 5));
-	// 	i++;
-	// 	j--;
-	// }
-
-	// double disorder;
-	// disorder = ft_compute_disorder(stack_a);
-	// printf("disorder = %f\n\n", disorder);
-
-	// printf("%s\n", "Stack before operation:");
-	// print_stack(stack_a);
-	// // rotate(&stack_a);
-
-	// // printf("%s\n", "Stack after operation:");
-
-	// // print_stack(stack_a);
-
-	// //moving first node to stack b
-	// stack_b = stack_a;
-	// stack_a = stack_a->next;
-	// stack_b->next = NULL;
-	// printf("%s\n", "Stack a:");
-	// print_stack(stack_a);
-	// printf("%s\n", "Stack b:");
-	// print_stack(stack_b);
+	stack_a = NULL;
+	flags = NULL;
+	if (!ft_check_input(argc, argv, &stack_a, &flags))
+	{
+		ft_free_stack(&stack_a);
+		free(flags);
+		return (ft_printf("Error\n"), 0);
+	}
+	print_stack(stack_a);
+	ft_free_stack(&stack_a);
+	free(flags);
 	return (0);
 }
