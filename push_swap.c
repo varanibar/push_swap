392d5d1c3a59e5d67ce53999b7890df59ca6a371
Veronica Aranibar Flores <varaniba@f0r4s10.codam.nl>
2026-05-04 12:37:41 +0200

    added one function t o initialize variables and one function to free all variables

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: varaniba <varaniba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/18 13:51:14 by varaniba      #+#    #+#                 */
-/*   Updated: 2026/05/04 12:32:38 by varaniba      ########   odam.nl         */
+/*   Updated: 2026/05/04 12:36:26 by varaniba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,15 @@ static int	ft_init_variables(t_stack **stack_a, t_stack **stack_b, t_flags **fla
 	if (!ft_init_t_flags(flags))
 		return(free(*stack_a), free(*stack_b), 0);
 	if (!ft_init_t_counter(counter))
-		return(free(*stack_a), free(*stack_b), free(flags), 0);
+		return(free(*stack_a), free(*stack_b), free(*flags), 0);
 	return(1);
 }
 
-static void	ft_free_all(t_stack **stack_a, t_stack **stack_b, t_flags **flags, t_ops **counter)
+static void	ft_free_all(t_stack **stack_a, t_flags **flags, t_ops **counter)
 {
 	ft_free_stack(stack_a);
-	ft_free_stack(stack_b);
-	free(flags);
-	free(counter);
+	free(*flags);
+	free(*counter);
 }
 
 int	main(int argc, char **argv)
@@ -43,12 +42,12 @@ int	main(int argc, char **argv)
 	if (!ft_init_variables(&stack_a, &stack_b, &flags, &counter))
 		return(0);
 	if (!ft_check_input(argc, argv, &stack_a, &flags) || ft_stack_size(stack_a) == 1)
-		return (ft_printf("Error\n"), ft_free_all(&stack_a, &stack_b, &flags, &counter), 0);
+		return (ft_printf("Error\n"), ft_free_all(&stack_a, &flags, &counter), 0);
 	// if (ft_stack_size(stack_a) == 1)
 	// 	return (ft_printf("Error\n"), ft_free_stack(&stack_a), free(flags), free(counter), 0);
 	disorder = ft_compute_disorder(stack_a);
 	if (!disorder)
-		return (ft_free_all(&stack_a, &stack_b, &flags, &counter), 0);
+		return (ft_free_all(&stack_a, &flags, &counter), 0);
 	if (flags->method == 1)
 		ft_selection_sort(&stack_a, &stack_b, counter);
 	else if (flags->method == 3)
@@ -57,7 +56,7 @@ int	main(int argc, char **argv)
 		ft_selection_sort(&stack_a, &stack_b, counter);
 	if (flags->bench == 1)
 		ft_printf("disorder = %.2f%%\n", disorder);
-	return (ft_free_all(&stack_a, &stack_b, &flags, &counter), 0, 0);
+	return (ft_free_all(&stack_a, &flags, &counter), 0, 0);
 }
 
 /*
