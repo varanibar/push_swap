597ff20072d1f3f77137401dddbc25cd10540e99
Veronica Aranibar Flores <varaniba@f1r2s19.codam.nl>
2026-05-05 12:39:21 +0200

    moved function ft_is_stack_sorted to a single file

---
+++ ft_is_stack_sorted.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_is_stack_sorted.c                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: varaniba <varaniba@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/05/05 12:35:23 by varaniba      #+#    #+#                 */
+/*   Updated: 2026/05/05 12:35:38 by varaniba      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "libft.h"
+
+int	ft_is_stack_sorted(t_stack **stack_a)
+{
+	t_stack	*current;
+	t_stack	*next;
+	int		size;
+	int		i;
+
+	current = *stack_a;
+	next = NULL;
+	size = ft_stack_size(*stack_a);
+	i = 0;
+	while (i < size - 1)
+	{
+		next = current->next;
+		if (current->val > next->val)
+			return (0);
+		current = next;
+		i++;
+	}
+	return (1);
+}
